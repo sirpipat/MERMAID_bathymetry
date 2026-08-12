872e3304b5b70ba2e25ca9be46ea6dc7d9717c90
Sirawich Pipatprathanporn <spipatprathanporn@ucsd.edu>
2026-08-12 16:25:44 +0700

    Allow you to type none as 'none'

---
+++ utility/none.m
@@ -0,0 +1,16 @@
+function str = none
+% str = NONE
+% 
+% Returns the word 'none' when you accidentally type 'none' without the
+% quotation marks.
+%
+% OUTPUT:
+% str       'none'
+%
+% SEE ALSO:
+% TRUE, FALSE
+% 
+% Last modified by spipatprathanporn@ucsd.edu, 08/12/2026
+
+str = 'none';
+end
